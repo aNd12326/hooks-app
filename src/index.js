3616e3ba8691b6e4e75a427aefb621482aa222ae
Anderson Ventosilla <andersonventosilla0@gmail.com>
2022-04-11 09:11:12 -0500

    114

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 
 
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import {HookApp} from './HookApp';
 // import {CounterWithCustomHook} from './components/01-useState/CounterWithCustomHook'
 
 ReactDOM.render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>,
   document.getElementById('root')
 );
